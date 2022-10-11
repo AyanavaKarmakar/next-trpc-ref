1eb9ae304869e350f3cf73be2db32256b1ae79c2
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-12 00:14:03 +0530

    feat: update query text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const { data } = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = trpc.example.hello.useQuery({
+    text: "from next-tRPC-ref repo",
+  });
 
   return (
     <main>
